05d552a38a65600209b91d1990dc126e69853703
sihle <gubug777@gmail.com>
2022-04-06 05:48:37 +0800

    add baseHeader.js

---
+++ header/baseHeader.js
@@ -0,0 +1,8 @@
+const headers = {
+  'Access-Control-Allow-Headers': 'Conten-Type, Content-Length, Authorization, X-Requested-With',
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'GET, POST, DELETE, PATCH, OPTIONS',
+  'Content-Type': 'application/json'
+};
+
+module.exports = headers;
