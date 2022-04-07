780d011b04c71aef4b17ba4fea7b986bde428830
sihle <gubug777@gmail.com>
2022-04-07 09:59:56 +0800

    [update] Refactor errHandler with code status

--- errHandler.js
+++ errHandler.js
@@ -1,11 +1,39 @@
 const headers = require('./header/baseHeader');
 
-function errHandler(res, statusCode, message) {
-  res.writeHead(statusCode, headers);
-  res.write(JSON.stringify({
+const errCodeInfos = {
+  default: 'connect error.',
+  400: {
+    40001: '非法資料格式',
+    40002: '資料格式錯誤',
+    40003: 'data not found.',
+  },
+  403: 'login error',
+  404: 'page not found.',
+}
+
+const errHandler = (res, statusCode, errCode) => {
+  // Build err msg
+  let errMsg = errCodeInfos['default'];
+  if (statusCode) errMsg = errCodeInfos[statusCode]
+  if (statusCode && errCode)
+    errMsg = errCodeInfos[statusCode][errCode]
+
+  let data = {
     status: 'false',
-    message
-  }));
+    errMsg
+  }
+
+  // 如果有 errCode 的話，更新輸出的錯誤訊息
+  if (errCode) {
+    data = {
+      ...data,
+      code: errCode,
+      errMsg: `error : ${errCodeInfos[statusCode][errCode]}`,
+    }
+  }
+
+  res.writeHead(statusCode, headers);
+  res.write(JSON.stringify(data));
   res.end();
 }
 
